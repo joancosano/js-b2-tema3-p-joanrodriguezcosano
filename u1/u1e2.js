f3b8b15e0458ef5275c53a73b7df5c675286e703
j2 <j2@mac-book-pro-joan.local>
2026-05-16 15:44:30 +0200

    terminado

--- u1/u1e2.js
+++ u1/u1e2.js
@@ -74,3 +74,55 @@ const PROJECT_LIST = [
 ];
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+
+function renderProjects(PROJECT_LIST){
+
+    const projectList = document.querySelector(".js-project-list");
+    const fragment = document.createDocumentFragment();
+
+    PROJECT_LIST.forEach(project => {
+
+        const template = document.querySelector("#tpl-project");
+        const clone = template.content.cloneNode(true);
+        const projectNode = clone.querySelector(".js-project");
+        projectNode.dataset.id = project.id;
+        projectNode.dataset.tags = project.tags.join(",");
+        projectNode.dataset.search = project.search.join(",");
+        projectNode.dataset.archived = project.archived;
+        clone.querySelector(".js-name").textContent = project.name;
+        clone.querySelector(".js-progress").textContent =project.progress;
+        clone.querySelector(".js-excerpt").innerHTML = project.excerpt;
+
+        const category = CATEGORY_LIST.find(
+            category => category.id === project.categoryId
+        );
+
+        clone.querySelector(".js-category").textContent =category.name;
+
+        if (project.archived){
+            projectNode.classList.add("archived")
+        }
+
+        if (project.progress === 100){
+            projectNode.classList.add("completed")
+        }
+
+        const tagsContainer = clone.querySelector(".js-tags");
+
+        project.tags.forEach((tag =>{
+             const tagTemplate = document.querySelector("#tpl-tag");
+             const tagClone = tagTemplate.content.cloneNode(true);
+             const taglink = tagClone.querySelector(".js-tag-link");
+             taglink.dataset.tag=tag;
+             taglink.textContent=tag;
+             tagsContainer.append(tagClone);
+        }));
+
+        fragment.append(clone);
+
+    });
+    projectList.append(fragment); 
+
+}
+
+renderProjects(PROJECT_LIST);
