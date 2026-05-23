b4a34c19952f1de2e16138482c61546edefab441
j2 <j2@mac-book-pro-joan.local>
2026-05-23 21:42:35 +0200

    terminado

--- u5/u5e1.js
+++ u5/u5e1.js
@@ -3,3 +3,21 @@
 // Enunciado disponible en u5e1.md / Enunciat disponible a u5e1.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+
+export class ClipboardApi {
+
+    // Constructor
+    constructor(clipboard = window.navigator.clipboard) {
+        this.clipboard = clipboard;
+    }
+
+    // Copiar texto
+    async copy(text) {
+        return await this.clipboard.writeText(text);
+    }
+
+    // Leer texto
+    async read() {
+        return await this.clipboard.readText();
+    }
+}
