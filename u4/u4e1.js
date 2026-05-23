437fceddba9935eb363d9a72662c92ce2f0225fb
j2 <j2@mac-book-pro-joan.local>
2026-05-23 21:36:10 +0200

    terminado

--- u4/u4e1.js
+++ u4/u4e1.js
@@ -4,3 +4,52 @@
 // Enunciado disponible en u4e1.md / Enunciat disponible a u4e1.md
 
 //Escribe aquí tu solución / escriviu aquí la vostra solució:
+
+
+class CookieApi {
+
+    static EXPIRING_DAYS = 365;
+
+    static expirationDate(nDays) {
+        const date = new Date();
+
+        date.setTime(
+            date.getTime() + nDays * 24 * 60 * 60 * 1000
+        );
+
+        return date.toUTCString();
+    }
+
+    constructor(document = window.document) {
+        this.document = document;
+    }
+
+    setCookie(
+        key,
+        value,
+        nDays = CookieApi.EXPIRING_DAYS
+    ) {
+
+        const jsonValue = JSON.stringify(value);
+
+        this.document[key] = jsonValue;
+    }
+
+    getCookie(key) {
+
+        const value = this.document[key];
+
+        if (value === undefined) {
+            return null;
+        }
+
+        return JSON.parse(value);
+    }
+
+    removeCookie(key) {
+        this.document[key] = JSON.stringify('');
+    }
+
+}
+
+export { CookieApi };
